109fe8872305c8f14fc9efe8f2ee35d7796691ae
Ahmed Elsayed <ahmedelsayed12004@gmail.com>
2025-09-23 14:39:18 +0300

    Initial commit

---
+++ src/test/java/com/gold/salarycalculation/SalaryCalculationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gold.salarycalculation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalaryCalculationServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
